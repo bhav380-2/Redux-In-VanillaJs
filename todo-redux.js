b207c203e808d3ab6be5672e9408c49a4622cd23
bhav380 <bhaweshrathour18@gmail.com>
2024-02-24 19:59:27 +0530

    actions and reducers : redux

---
+++ todo-redux.js
@@ -0,0 +1,55 @@
+const redux = require("redux");
+
+//Actions
+const ADD_TODO = "ADD TODO";
+const TOGGLE_TODO = "Toggle TODO";
+
+// ACTIONS Creators
+const addTodo = (text)=>({
+    text,
+    type:ADD_TODO
+});
+
+const toggleTodo = (index)=>({
+    index,
+    type:TOGGLE_TODO
+});
+
+//Initial state
+const initialState={
+    todos:[]
+}
+
+
+//Reducers
+function todoReducer(state=initialState,action){
+
+    switch(action.type){
+        case "ADD_TODO":
+            return{
+                ...state,
+                todos:[
+                    ...state.todos,
+                    {
+                        text:action.text,
+                        completed:false
+                    }
+                ]
+            }
+        case "TOGGLE_TODO":
+            return{
+                ...state,
+                todos:todos.map((todo,i)=>{
+                    if(i==action.index){
+                        todo.completed=!todo.completed
+                    }
+                    return todo;
+                })
+            }
+
+        default:
+            return state;
+    }
+}
+
+
